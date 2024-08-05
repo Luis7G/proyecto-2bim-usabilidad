31cef136396c58c96288b6cdd2a92dc2a85ec634
Erikmcl <erik.chalacama@epnedu.ec>
2024-08-04 22:48:54 -0500

    fix: correccion footer

--- src/components/QuestionFooter.jsx
+++ src/components/QuestionFooter.jsx
@@ -1,22 +1,7 @@
 function QuestionFooter({ handlePrevious, handleNext }) {
   return (
-    <footer className="flex justify-between p-4 bg-gray-200">
-      <button
-        onClick={handlePrevious}
-        className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
-        tabIndex="0"
-        aria-label="Ir a la pregunta anterior"
-      >
-        Anterior
-      </button>
-      <button
-        onClick={handleNext}
-        className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
-        tabIndex="0"
-        aria-label="Ir a la siguiente pregunta"
-      >
-        Siguiente
-      </button>
+    <footer >
+     
     </footer>
   );
 }
